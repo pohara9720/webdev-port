48ac77876d40acaca23c714fef693feead15eb20
Patrick O'Hara <Patrick@Patricks-MacBook-Pro.local>
2017-07-27 15:15:49 -0700

    asd

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+$('.smooth').on('click', function() {
+    $.smoothScroll({
+        scrollElement: $('body'),
+        scrollTarget: '#' + this.id
+    });
+    
+    return false;
+});
+
+});
